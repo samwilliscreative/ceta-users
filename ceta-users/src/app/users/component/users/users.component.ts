6ac5dbb44ca020f230b1fe8015f09c04932775d4
Sam Willis <sam.willis@mab.org.uk>
2021-02-24 21:25:13 +0000

    Add user list component

---
+++ ceta-users/src/app/users/component/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../service/user-model.interface';
+import { UsersService } from '../../service/users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+  users$: Observable<User[]>;
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.usersService.getUsers();
+    this.users$ = this.usersService.users$;
+  }
+
+}
